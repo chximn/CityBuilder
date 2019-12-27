5bb94f26cbeed5eddb2073cae1cb18e486a65c3d
Anis BOUCHEKHIMA <bouchekhima.anis@gmail.com>
2019-12-27 15:29:06 +0100

    add error reporting to expression_binaire

--- expressions/expressionBinaire.cc
+++ expressions/expressionBinaire.cc
@@ -9,17 +9,18 @@ ExpressionBinaire::ExpressionBinaire(ExpressionPtr gauche, ExpressionPtr droite,
 int ExpressionBinaire::calculer(const Contexte& contexte) const {
     int gauche = _gauche->calculer(contexte), droite = _droite->calculer(contexte);
     switch (_op) {
-        case OperateurBinaire::plus: return gauche + droite;
-        case OperateurBinaire::moins: return gauche - droite;
-        case OperateurBinaire::divise: return gauche / droite;
+        case OperateurBinaire::plus:      return gauche + droite;
+        case OperateurBinaire::moins:     return gauche - droite;
+        case OperateurBinaire::divise:    if (droite == 0) error::report("Division par zéro est interdite");
+		                                  return gauche / droite;
         case OperateurBinaire::multiplie: return gauche * droite;
-        case OperateurBinaire::equ: return gauche == droite;
-        case OperateurBinaire::inf: return gauche < droite;
-        case OperateurBinaire::sup: return gauche > droite;
-        case OperateurBinaire::inf_equ: return gauche <= droite;
-        case OperateurBinaire::sup_equ: return gauche >= droite;
-        case OperateurBinaire::et:  return gauche && droite;
-        case OperateurBinaire::ou:  return gauche || droite;
+        case OperateurBinaire::equ:       return gauche == droite;
+        case OperateurBinaire::inf:       return gauche < droite;
+        case OperateurBinaire::sup:       return gauche > droite;
+        case OperateurBinaire::inf_equ:   return gauche <= droite;
+        case OperateurBinaire::sup_equ:   return gauche >= droite;
+        case OperateurBinaire::et:        return gauche && droite;
+        case OperateurBinaire::ou:        return gauche || droite;
         default: return 0;
     }
 }
